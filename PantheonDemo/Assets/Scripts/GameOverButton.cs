894d7d1c0ce618ba3bfdc66f200dacfd6be6dc9a
Barış Köklü <30669414+bariskoklu@users.noreply.github.com>
2021-01-11 23:29:20 +0300

    Gameover buttonunun fonksiyonalitesi yapıldı

---
+++ PantheonDemo/Assets/Scripts/GameOverButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverButton : MonoBehaviour
+{
+    public BoolType isGameOver;
+    // Start is called before the first frame update
+
+    public void RestartGame()
+    {
+        isGameOver.value = false;
+        SceneManager.LoadScene(0);
+    }
+    
+}
